b02b9f2b262c35788381cffc12e3a0bd9e75567f
John Lombardi <jlombard314@gmail.com>
2022-01-27 20:27:35 -0700

    redoing files

--- read_in_spatial.py
+++ read_in_spatial.py
@@ -1,7 +1,6 @@
 ##read in shapefile and convert to something for leaflet
 import geopandas
-local_file = 'C:/Users/jlomb/Documents/Vedauwoo Guidebook/'
 
-shp_file = geopandas.read_file(local_file + 'vedauwoo.shp')
+shp_file = geopandas.read_file('assets/vedauwoo.shp')
 
-shp_file.to_file('vedauwoo.geojson', driver = 'GeoJSON')
+shp_file.to_file('assets/vedauwoo.geojson', driver = 'GeoJSON')
