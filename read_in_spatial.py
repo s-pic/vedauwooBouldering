721b53206f9b52eda153624affcef000611c0d42
John Lombardi <jlombard314@gmail.com>
2022-01-27 20:44:27 -0700

    hacky way of doing it but it work i guess

--- read_in_spatial.py
+++ read_in_spatial.py
@@ -4,3 +4,5 @@ import geopandas
 shp_file = geopandas.read_file('assets/vedauwoo.shp')
 
 shp_file.to_file('assets/vedauwoo.geojson', driver = 'GeoJSON')
+
+##NOTE: manually save as geojson, then create a json file where you add var name{} to it
